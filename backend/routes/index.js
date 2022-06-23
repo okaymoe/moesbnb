5adb61b933a7c57615f16a4d0655bc222cc50432
Moe <moe62197@gmail.com>
2022-06-23 19:04:21 -0400

    initial frontend

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,11 +1,6 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/hello/world', function(req, res) {
-  res.cookie('XSRF-TOKEN', req.csrfToken());
-  res.send('Hello World!');
-});
-
 const apiRouter = require('./api');
 
 router.use('/api', apiRouter);
