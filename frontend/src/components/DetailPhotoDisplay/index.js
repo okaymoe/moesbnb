a6b796bfd646bb08652a902c640fdc9e9ebfdaa0
Moe <moe62197@gmail.com>
2022-07-01 11:30:50 -0400

    DPD

--- frontend/src/components/DetailPhotoDisplay/index.js
+++ frontend/src/components/DetailPhotoDisplay/index.js
@@ -21,26 +21,9 @@ useEffect(() => {
         <figure
           className='listing-photos__main-image card__image'
           style={{ backgroundImage: `url( ${image.url} )` }} />
-        </div>
+      </div>
     ))}
     </div>
-    // <div className='listing-photos container'>
-    //   <div className='listing-photos__main'>
-    //     <figure
-    //       className='listing-photos__main-image card__image'
-    //       style={{ backgroundImage: `url( ${firstImage} )` }} />
-    //   </div>
-    //   <ul className='listing-photos__sub'>
-    //     <li className='listing__photos-sub' ><figure
-    //       className='listing-photos__image listing-photos__photo1'
-    //       style={{ backgroundImage: `url( ${display[0].url} )` }} /></li>
-    //     <li className='listing__photos-sub'> 
-    //       <figure
-    //         className='listing-photos__image listing-photos__photo2'
-    //         style={{ backgroundImage: `url( ${display[1].url} )` }} />
-    //     </li>
-    //   </ul>
-    // </div>
   )
 }
 
