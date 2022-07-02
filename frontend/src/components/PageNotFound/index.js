c442bd3bc6ae4c5f2d287926027826bd713eeacd
Moe <moe62197@gmail.com>
2022-07-02 11:09:24 -0400

    PNF edit

--- frontend/src/components/PageNotFound/index.js
+++ frontend/src/components/PageNotFound/index.js
@@ -5,9 +5,10 @@ const PageNotFound = () => {
   return (
     <div className='redirect'>
       <div className='redirect__card form__card'>
-        <p className='header-title'>Looks like we don't have exactly what you're looking for...</p>
         <ul>
-          <li className='redirect__text'><Link to={`/`}>But we might have something else</Link></li>
+        <img id="brokenhome" src="https://cdn3.iconfinder.com/data/icons/love-and-romance-12/24/_broken_home-512.png" alt="notfound"/>  
+        <p className='header-title'>Sorry, we can't find what you're looking for...</p>
+          <li className='redirect__text'><Link to={`/`}>Home Sweet Home</Link></li>
         </ul>
       </div>
     </div>
